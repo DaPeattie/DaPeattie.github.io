f11b7eeb5a331433f7ba7fbbae1d0c876f330d0f
David <david.a.peattie@gmail.com>
2018-07-22 15:21:01 +1200

    test

--- javascript/shared.js
+++ javascript/shared.js
@@ -1,1 +1,11 @@
 // JavaScript Document
+
+var thisPath = window.location.pathname.split('/');
+var thisFolder = 'Home';
+
+if(thisPath.length > 1){
+	thisFolder = thisPath[1];
+}
+$('#webTitle').html(thisFolder);
+
+
